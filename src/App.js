151b062b01bc42448eb0bb3ad388347a358c0c79
rominapenayo <romina.penayo1@gmail.com>
2022-09-02 23:56:38 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import { useState } from 'react';
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+
+function App() {
+
+  const [carrito, setCarrito] = useState({
+    stock: 0
+  })
+
+  return (
+    <div className="App">
+      <Cabecera carrito={carrito}/>
+      <Listado carrito={carrito} setCarrito={setCarrito}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,40 @@
+
+// El componente Item no tiene componentes hijos.
+// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
+// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
+// Maqueta de Item:
+//    h3
+//    p
+//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
+//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+import React, { useState } from 'react'
+
+export default function Item({item, carrito, setCarrito}) {
+
+
+  const [cantidad, setCantidadItem] = useState(item.stock)
+
+  const comprar = () =>{
+      setCantidadItem(cantidad - 1)
+      setCarrito({
+          stock: carrito.stock + 1
+      })
+  }
+  return (
+    <div className='producto'>
+       <h3>{item.producto.nombre}</h3>
+       <p>{item.producto.descripcion}</p>
+       <h5>En Stock: {cantidad > 0 ? cantidad : <span>agotado</span>}</h5>
+        {cantidad > 0 ? 
+            <button onClick={comprar}>
+                COMPRAR
+            </button> 
+            : 
+            <button disabled>
+                SIN STOCK
+            </button>
+        }
+    </div>
+  )
+}
